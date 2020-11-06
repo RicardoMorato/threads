8b27e56e2d91fd0d773f3ee030404fea4c77d0f2
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-11-06 20:01:10 -0300

    feat(q4): Added Structs.

--- q4/q4.c
+++ q4/q4.c
@@ -3,6 +3,17 @@
 #include <pthread.h>
 
 #define N 5 // Número de processadores/núcleos do sistema, portanto, também representa a quantidade de threads do programa
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 10 // Tamanho máximo do Buffer
+
+typedef struct param { // Struct pedida pela questão que irá servir como parâmetro de uma função em uma execução
+  int a, b, c;
+} Param;
+
+typedef struct Execucao { // Para cada execucao agendada na funcao agendarExecucao, será criada uma instância desta Struct
+  void *func;
+  Param funcParams;
+  int execucaoId;
+  int threadId;
+};
 
 int main(int argc, char *argv[]) {return 0;}
